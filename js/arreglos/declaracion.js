1b08116bc2fe0c3dba2a5b24ff1d86bc228f2747
Diego Roble <diego.roble@zabud.com.co>
2021-10-14 18:58:39 -0500

    creación de arreglos

---
+++ js/arreglos/declaracion.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nombres = new Array('Diego', 'Carlos', 'Alejandra');
+const deTodo = ['Xiaomi', 20303, true, null, undefined, 21321, { nombre: 'Diego', edad: 232 }];
+console.log(numeros);
+console.log(nombres);
+console.log(deTodo);
