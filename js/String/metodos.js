086c67620dd3b6386e259ceefd81259cf581c76c
Diego Roble <diego.roble@zabud.com.co>
2021-10-13 23:31:34 -0500

    Convertir mayúscula y minúscula. Dividir cadena de texto y repetir texto

--- js/String/metodos.js
+++ js/String/metodos.js
@@ -21,3 +21,17 @@ console.log(frase.replace('Sólo', 'texto nuevo'));
 console.log(frase.slice(0, 8));
 console.log(frase.substring(0, 8));
 console.log(nombre.trim().charAt(0));
+
+
+//Repetir una cadena de texto
+const individuo = "Juana";
+
+console.log(individuo.repeat(10));
+
+//dividir una cadena
+console.log(individuo.split('a'));
+
+
+//Convertir cadena a mayúsula y minúscula
+console.log(individuo.toLowerCase());
+console.log(individuo.toUpperCase());
