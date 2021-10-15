0638ddfa84fac5b2883ffce0f48b50b8e045e251
Diego Roble <diego.roble@zabud.com.co>
2021-10-15 14:31:05 -0500

    función de flecha

--- js/funciones/return.js
+++ js/funciones/return.js
@@ -1,7 +1,7 @@
 const funcionJs = (n1, n2) => {
 
     return (parametro1, parametro2) => {
-        return (elevado) => `Hola mundo : ${Math.pow(((n1 + n2) * (parametro1 + parametro2)), elevado)}`;
+        return (elevado) => `<h1>Hola mundo : ${Math.pow(((n1 + n2) * (parametro1 + parametro2)), elevado)}</h1>`;
     };
 };
 
@@ -11,9 +11,7 @@ function mostarInfomarcion() {
     contenido.addEventListener('click', mostrarResultado);
 }
 
-const mostrarResultado = (event) => {
-    event.target.firstElementChild.textContent = funcionJs(2, 2)(2, 3)(2);
-};
+const mostrarResultado = (event) => event.target.firstElementChild.textContent = funcionJs(2, 2)(2, 3)(2);
 
 
 mostarInfomarcion();
