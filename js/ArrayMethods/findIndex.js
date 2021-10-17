5b03366afc711964b2fd1c647203b75c973b1900
Diego Roble <diego.roble@zabud.com.co>
2021-10-16 22:29:14 -0500

    método reduce y findIndex

---
+++ js/ArrayMethods/findIndex.js
@@ -0,0 +1,6 @@
+const arr = ['XIaomi', 'Samsung', 'Apple', 'Poco Phone'];
+
+
+//Retorna el índice del elemento en el arreglo.
+const indice = arr.findIndex(marca => marca === 'Apple');
+console.log(indice);
---
+++ js/ArrayMethods/reduce.js
@@ -0,0 +1,7 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+const sumatoria = numeros.reduce((valor1, valor2) => valor1 + valor2);
+
+console.log(sumatoria);
