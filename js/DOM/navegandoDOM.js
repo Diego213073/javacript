73dd8340e72810604dc319784f3023811bb27b71
Diego Roble <diego.roble@zabud.com.co>
2021-10-19 00:55:45 -0500

    todos los cambios faltantes

---
+++ js/DOM/navegandoDOM.js
@@ -0,0 +1,5 @@
+const navegacion = document.querySelector('.contenido');
+
+
+console.log(navegacion.childNodes); //Retorna una lista de los hijos y toma encuenta los espacios en blanco del html
+console.log(navegacion.children); //Solamente retorna la lista de elemntos descartando los espacios en blanco del HTML
